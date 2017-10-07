478666d38ac2814f6ff41188199574dd1d7a0062
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2017-10-07 14:29:53 -0300

    Creates create_todo_list endpoint

--- app.py
+++ app.py
@@ -10,8 +10,18 @@ def welcome(name=None):
         return {'message': 'Welcome to API Star!'}
     return {'message': 'Welcome to API Star, %s!' % name}
 
+
+def create_todo_list(title=None):
+	todo_list = {
+		'title': title
+	}
+	todo_lists.append(todo_list)
+	return http.Response({}, status=201)
+
+
 routes = [
     Route('/', 'GET', welcome),
+    Route('/create_todo_list', 'POST', create_todo_list),
     Include('/docs', docs_urls),
     Include('/static', static_urls)
 ]
